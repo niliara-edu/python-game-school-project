6074cf3d6b86ab5854602c2509635b547f384e1d
niliara-edu <nil.jimeno@estudiant.fjaverianas.com>
2024-05-16 18:24:14 +0200

    hotfix (again)

--- scenes/highscores.py
+++ scenes/highscores.py
@@ -39,7 +39,7 @@ class Highscores():
 
         for i in range(0, len(self.highscores)):
             time = self.highscores[i][2] // 30
-            pyxel.text(8, y,  str(i), 9)
+            pyxel.text(8, y,  str(i+1), 9)
             pyxel.text(18, y, self.highscores[i][0], 9)
             pyxel.text(54, y, str(self.highscores[i][1]), 9)
             pyxel.text(76, y, str(time//60) + ":" + str(time%60), 9)
