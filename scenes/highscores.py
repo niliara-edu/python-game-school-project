576647fc3d6adaa3d9cda9550229613f08e0acda
niliara-edu <nil.jimeno@estudiant.fjaverianas.com>
2024-05-17 10:21:58 +0200

    change in highscores

--- scenes/highscores.py
+++ scenes/highscores.py
@@ -39,10 +39,10 @@ class Highscores():
 
         for i in range(0, len(self.highscores)):
             time = self.highscores[i][2] // 30
-            pyxel.text(8, y,  str(i+1), 9)
+            pyxel.text(2, y,  str(i+1), 9)
             pyxel.text(18, y, self.highscores[i][0], 9)
             pyxel.text(54, y, str(self.highscores[i][1]), 9)
-            pyxel.text(76, y, str(time//60) + ":" + str(time%60), 9)
+            pyxel.text(76, y, str(time//60) + ":" + "{0:0=2d}".format(time%60), 9)
             y += 10
         
         pyxel.text(6, y, "Press ENTER to go back", 12)
